e007348b178f1849595e1c6cd49059f38a60573a
Marcus-Vinicius <91153529+MarcusVinicius1@users.noreply.github.com>
2024-08-27 10:39:24 -0300

    Update funcoes.py

--- Python/Bot span/funcoes.py
+++ Python/Bot span/funcoes.py
@@ -1,24 +1,33 @@
 import pyautogui # pip install PyAutoGUI
 from time import sleep
+from tkinter import messagebox
 
 def LoadArquivo(ARQUIVO):
-	arquivo = open(ARQUIVO, encoding='utf8')
+	try:
+		arquivo = open(ARQUIVO, encoding='utf8')
 
-	for fraseAquivo in arquivo:
-		pyautogui.typewrite(fraseAquivo)
-		pyautogui.press('enter')
+		for fraseAquivo in arquivo:
+			pyautogui.typewrite(fraseAquivo)
+			pyautogui.press('enter')
+	
+	except ValueError as err:
+		messagebox.showerror("Error: ".format(err), "Não foi possivel carregar o arquivo!: {}".format(ARQUIVO))
 
 def LoadTexto(REPETICAO):
-	texto = input('Seu texto: ')
-	LoopMarcado = REPETICAO
+	try:
+		texto = input('Seu texto: ')
+		LoopMarcado = REPETICAO
 
-	print('\n5 segundos pra iniciar\n')
-	sleep(5)
+		print('\n5 segundos pra iniciar\n')
+		sleep(5)
 
-	while True:
-		pyautogui.typewrite(texto)
-		pyautogui.press('enter')
-		LoopMarcado -= 1
+		while True:
+			pyautogui.typewrite(texto)
+			pyautogui.press('enter')
+			LoopMarcado -= 1
 
-		if LoopMarcado == 0:
-			break
+			if LoopMarcado == 0:
+				break
+	
+	except ValueError as err:
+		messagebox.showerror("Error: ".format(err), "Não foi possivel carregar o seu texto!: {}".format(texto)) 
